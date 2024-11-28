25e3a6fb3da8baa978869c0b96a04ee05ae4d055
Roy <roy.li@nrel.gov>
2024-11-27 19:04:21 -0700

    wind-watts init

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout';
+import About from './components/About';
+import Contact from './components/Contact';
+import MapView from './components/MapView';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { path: '/', element: <MapView /> },
+      { path: '/about', element: <About /> },
+      { path: '/contact', element: <Contact /> },
+    ]
+  }
+]
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
