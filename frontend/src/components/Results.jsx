09ba80a2aff848a71b334e07d2f2ae75586b5906
Roy <roy.li@nrel.gov>
2024-12-09 11:16:17 -0700

    minor adjustment for the close button

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -47,7 +47,7 @@ const Results = ({ openResults, handleClose,  currentPosition, hubHeight, powerC
         <Modal open={openResults} onClose={handleClose}>
             <Box sx={{ 
             position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', 
-            width:800, minWidth: 600, bgcolor: 'background.paper', p: 4 }}>
+            width:800, minWidth: 600, bgcolor: 'background.paper', p: 3, pt: 6 }}>
                 <IconButton
                     aria-label="close"
                     onClick={handleClose}
@@ -59,7 +59,7 @@ const Results = ({ openResults, handleClose,  currentPosition, hubHeight, powerC
                 >
                     <Close />
                 </IconButton>
-                <Accordion sx={{ mt: 3}} >
+                <Accordion >
                     <AccordionSummary
                         expandIcon={<ExpandMore />}
                         aria-controls="summary-panel-content"
