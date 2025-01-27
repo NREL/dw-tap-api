63b62ab66f62206c53fbc8f018c7b02ce070bbf8
Roy <roy.li@nrel.gov>
2025-01-27 09:22:12 -0700

    store config secret in tmp file as config file

--- windwatts-api/config_manager.py
+++ windwatts-api/config_manager.py
@@ -1,9 +1,10 @@
 import os
 import json
 import boto3
+import tempfile
 
 class ConfigManager:
-    def __init__(self, secret_arn_env_var: str, local_config_path: str = None):
+    def __init__(self, secret_arn_env_var: str, local_config_path: str = None, region_name="us-west-2"):
         """
         Initialize the ConfigManager with the ARN of the secret.
 
@@ -12,26 +13,32 @@ class ConfigManager:
         """
         self.secret_arn = os.getenv(secret_arn_env_var)
         self.local_config_path = local_config_path
-        self.client = boto3.client('secretsmanager')
+        self.client = boto3.client('secretsmanager', region_name=region_name)
 
-    def get_config(self):
+    def get_config(self) -> str:
         """
-        Retrieve the secret from AWS Secrets Manager.
+        Retrieve the secret from AWS Secrets Manager or the local configuration file.
 
-        :return: The secret as a dictionary.
+        :return: The path to the configuration file.
         """
         # Try to retrieve the secret from AWS Secrets Manager
         if self.secret_arn:
             try:
                 response = self.client.get_secret_value(SecretId=self.secret_arn)
                 secret = response['SecretString']
-                return json.loads(secret)
+                config_data = json.loads(secret)
+                
+                # Save the secret to a temporary file
+                temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".json")
+                with open(temp_file.name, 'w') as f:
+                    json.dump(config_data, f)
+                return temp_file.name
             except self.client.exceptions.ClientError as e:
                 print(f"Unable to retrieve secret: {e}")
-        # fallback: try to retrieve the secret from the local configuration file
-        try:
-            with open(self.local_config_path, 'r') as f:
-                return json.load(f)
-        except FileNotFoundError as e:
-            print(f"Local configuration file not found: {e}")
-        return {}
+        
+        # Fallback: return the path to the local configuration file
+        if self.local_config_path and os.path.exists(self.local_config_path):
+            print("Local configuration file found.")
+            return self.local_config_path
+        else:
+            raise FileNotFoundError("Local configuration file not found and unable to retrieve secret from AWS Secrets Manager.")
