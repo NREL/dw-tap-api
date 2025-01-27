3f1f8c57b50a7098aeb0b7ae2b5a9917587e0a99
David Whiteside <david.whiteside@nrel.gov>
2025-01-27 14:29:33 -0700

    configured appfleet2 build settings

--- windwatts-ui/release.sh
+++ windwatts-ui/release.sh
@@ -10,11 +10,11 @@ export CONFIG_FILE_URL=https://github.nrel.gov/nrel-cloud-computing/appfleet-ui/
 export APPFLEET_BUILD_ARGS='{"main": {"image_detail": "./appfleet-config/mainDetail.json", "dockerfile": "./Dockerfile", "ecr_repo": "$(REGISTRY-IDS).dkr.ecr.us-west-2.amazonaws.com/nrel-windwatts-ui-${APPFLEET_RELEASE_NAME}", "target_arg": ""}}'
 export CACHE_S3_BUCKET=nrel-windwatts-ui-codebuild
 # aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 991404956194.dkr.ecr.us-west-2.amazonaws.com
-#make -f "$MAKEFILE_PATH" V=1 buildx
-make -f "$MAKEFILE_PATH" V=1 build push --provenance=false
+make -f "$MAKEFILE_PATH" V=1 buildx
+#make -f "$MAKEFILE_PATH" V=1 build push
 # Run the generate_appfleet_tag_overrides.sh script
 echo "Running generate_appfleet_tag_overrides.sh to populate APPFLEET_TAG_OVERRIDES..."
-#APPFLEET_TAG_OVERRIDES=$(./appfleet-config/generate_appfleet_tag_overrides.sh)
+APPFLEET_TAG_OVERRIDES=$(./appfleet-config/generate_appfleet_tag_overrides.sh)
 # Output the populated APPFLEET_TAG_OVERRIDES
-#echo "APPFLEET_TAG_OVERRIDES: $APPFLEET_TAG_OVERRIDES"
-#make -f "$MAKEFILE_PATH" V=1 APPFLEET_TAG_OVERRIDES="$APPFLEET_TAG_OVERRIDES" deploy
+echo "APPFLEET_TAG_OVERRIDES: $APPFLEET_TAG_OVERRIDES"
+make -f "$MAKEFILE_PATH" V=1 APPFLEET_TAG_OVERRIDES="$APPFLEET_TAG_OVERRIDES" deploy
