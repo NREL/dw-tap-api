a4481c04993421488f4b640aadff0d2c373092c0
David Whiteside <david.whiteside@nrel.gov>
2025-01-27 16:44:43 -0700

    api

--- windwatts-api/release.sh
+++ windwatts-api/release.sh
@@ -7,7 +7,7 @@ export APPFLEET_DEPLOY_VERSION=2.1.3-alpine-e1f2893
 export BASE_IMAGE_TAG=3.11.4-slim-buster
 export CONFIG_FILE_URL=https://github.nrel.gov/nrel-cloud-computing/appfleet-api/raw/main/appfleet-config/appfleet.yml
 # Properly format the JSON for APPFLEET_BUILD_ARGS
-export APPFLEET_BUILD_ARGS='{"main": {"image_detail": "./appfleet-config/mainDetail.json", "dockerfile": "./Dockerfile", "ecr_repo": "$(REGISTRY-IDS).dkr.ecr.us-west-2.amazonaws.com/nrel-windwatts-api", "target_arg": ""}}'
+export APPFLEET_BUILD_ARGS='{"main": {"image_detail": "./appfleet-config/mainDetail.json", "dockerfile": "./Dockerfile", "ecr_repo": "$(REGISTRY-IDS).dkr.ecr.us-west-2.amazonaws.com/nrel-windwatts-api-${APPFLEET_RELEASE_NAME}", "target_arg": ""}}'
 export CACHE_S3_BUCKET=nrel-windwatts-api-codebuild
 # aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 991404956194.dkr.ecr.us-west-2.amazonaws.com
 make -f "$MAKEFILE_PATH" V=1 buildx
