d8cd66792288b88fc72848fdd767dff30172e255
Roy <roy.li@nrel.gov>
2025-03-05 11:18:59 -0700

    update windwatts data version and methods

--- windwatts-api/app/data_fetchers/athena_data_fetcher.py
+++ windwatts-api/app/data_fetchers/athena_data_fetcher.py
@@ -1,5 +1,5 @@
 from .abstract_data_fetcher import WTKDataFetcher
-from windwatts_data import WTKLedClient1224
+from windwatts_data import WindwattsWTKClient
 
 class AthenaDataFetcher(WTKDataFetcher):
     def __init__(self, athena_config):
@@ -9,9 +9,9 @@ class AthenaDataFetcher(WTKDataFetcher):
         Args:
             athena_config (str): Path to the Athena configuration file.
         """
-        self.wtk_client = WTKLedClient1224(config_path=athena_config)
+        self.wtk_client = WindwattsWTKClient(config_path=athena_config)
 
-    def fetch_data(self, lat: float, lng: float, height: int):
+    def fetch_data(self, lat: float, lng: float, height: int, avg_type: str = 'global'):
         """
         Fetch data from Athena using the WTKLedClient.
 
@@ -23,5 +23,12 @@ class AthenaDataFetcher(WTKDataFetcher):
         Returns:
             dict: A dictionary containing the fetched data.
         """
-        filtered_data = self.wtk_client.fetch_windwatts_data(lat=lat, long=lng, height=height)
+        filtered_data = None
+        if avg_type == 'global':
+            filtered_data = self.wtk_client.fetch_global_avg_at_height(lat=lat, long=lng, height=height)
+        elif avg_type == 'yearly':
+            filtered_data = self.wtk_client.fetch_yearly_avg_at_height(lat=lat, long=lng, height=height)
+        elif avg_type == 'monthly':
+            filtered_data = self.wtk_client.fetch_monthly_avg_at_height(lat=lat, long=lng, height=height)
+        
         return filtered_data
