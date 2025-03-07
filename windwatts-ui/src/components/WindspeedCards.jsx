6d7a67aa9bace20dfdea5010bf4dd0ac297c5e9a
Shawn Petros <shawn.petros@gmail.com>
2025-03-07 15:46:43 -0800

    adds the unit conversion feature

--- windwatts-ui/src/components/WindspeedCards.jsx
+++ windwatts-ui/src/components/WindspeedCards.jsx
@@ -1,6 +1,6 @@
 import useSWR from "swr";
 import ResultCard from "./ResultCard";
-import { getWindspeedByLatLong, getEnergyProduction } from "../services/api";
+import { getEnergyProduction, getWindspeedByLatLong } from "../services/api";
 import { convertOutput, convertWindspeed, getWindResource } from "../utils";
 import { Typography, Grid2, Stack, Skeleton } from "@mui/material";
 import { useContext } from "react";
@@ -8,34 +8,38 @@ import { UnitsContext } from "../providers/UnitsContext";
 
 export default function WindspeedCards({ lat, lng, height, powerCurve }) {
   const { units } = useContext(UnitsContext);
-  const shouldFetch = lat && lng;
+  const shouldFetch = lat && lng && height;
 
-  const { isLoading: isLoadingWindspeed, data: windspeedData, error: windspeedError } = useSWR(
-    // use lat, lng, height as obj cache key; see https://swr.vercel.app/docs/arguments#passing-objects
-    shouldFetch ? { lat, lng, height } : null,
-    getWindspeedByLatLong
-  );
-
-  const { isLoading: isLoadingEnergy, data: energyData, error: energyError } = useSWR(
-    shouldFetch ? { lat, lng, height, powerCurve } : null,
+  const {
+    isLoading: energyIsLoading,
+    data: energy,
+    error: energyError,
+  } = useSWR(
+    shouldFetch && powerCurve ? { lat, lng, height, powerCurve } : null,
     getEnergyProduction
   );
+
+  const {
+    isLoading: windspeedIsLoading,
+    data: windspeed,
+    error: windspeedError,
+  } = useSWR(shouldFetch ? { lat, lng, height } : null, getWindspeedByLatLong);
+
+  const isLoading = energyIsLoading || windspeedIsLoading;
+  const error = energyError || windspeedError;
+  const data = energy && windspeed ? { ...energy, ...windspeed } : null;
 
   const dataCards = [
     {
       title: "Average Wind Speed",
       subheader: "Average wind speed at selected height",
-      data: convertWindspeed(windspeedData?.global_avg, units.windspeed),
-      details: [
-        "Average wind speed estimated using WTK dataset",
-        `Wind speed data is taken from a nearby location at lat: and lng: at a height of ${height} meters`,
-        "Average wind speed is calculated over a 20-year period",
-      ],
+      data: convertWindspeed(data?.global_avg, units.windspeed),
+      details: [],
     },
     {
       title: "Wind Resource",
       subheader: "Broad measure of how much wind is available",
-      data: getWindResource(windspeedData?.global_avg),
+      data: getWindResource(data?.global_avg),
       details: [
         "Low refers to wind speeds below 3.00 m/s.",
         "Moderate refers to speeds between 3.00 m/s and 5.00 m/s.",
@@ -45,40 +49,37 @@ export default function WindspeedCards({ lat, lng, height, powerCurve }) {
     {
       title: "Production",
       subheader: "Estimated annual production potential",
-      data: convertOutput(energyData?.energy_production, units.output),
-      details: [energyData?.totalOutputDisclaimer],
+      data: convertOutput(data.energy_production, units.output),
+      details: [],
     },
   ];
-
-  const isLoading = isLoadingWindspeed || isLoadingEnergy;
-  const error = windspeedError || energyError;
 
   return (
     <Stack spacing={2} sx={{ marginTop: 4 }}>
       {error && (
         <Grid2>
           <Typography marginTop={2} variant="body1" color="error" gutterBottom>
-            There was an error loading data: {error.message}
+            There was an error loading data: {error?.message}
           </Typography>
         </Grid2>
       )}
       {isLoading ? (
         <Grid2>
-          <Skeleton variant="rounded" animation="wave" height={192.5} />
+          <Skeleton variant="rounded" animation="wave" height={180} />
           <Skeleton
             sx={{ marginTop: 2 }}
             variant="rounded"
             animation="wave"
-            height={192.5}
+            height={180}
           />
           <Skeleton
             sx={{ marginTop: 2 }}
             variant="rounded"
             animation="wave"
-            height={192.5}
+            height={180}
           />
         </Grid2>
-      ) : windspeedData && energyData ? (
+      ) : data ? (
         dataCards.map((data, index) => (
           <Grid2 key={"result_card_" + index}>
             <ResultCard data={data} isLoading={isLoading} />
