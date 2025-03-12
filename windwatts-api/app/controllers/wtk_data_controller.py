3461e7b3d9dbf1cba82ef5e50bd2d63cf4559e41
Shawn Petros <shawn.petros@gmail.com>
2025-03-12 16:17:30 -0700

    feat(windwatts-api): hardcode api data for now

--- windwatts-api/app/controllers/wtk_data_controller.py
+++ windwatts-api/app/controllers/wtk_data_controller.py
@@ -1,34 +1,35 @@
+import random
 from fastapi import APIRouter, HTTPException
 
 # commented out the data functions until I can get local athena_config working
-from app.config_manager import ConfigManager
-from app.data_fetchers.s3_data_fetcher import S3DataFetcher
-from app.data_fetchers.athena_data_fetcher import AthenaDataFetcher
-from app.data_fetchers.database_data_fetcher import DatabaseDataFetcher
-from app.data_fetchers.data_fetcher_router import DataFetcherRouter
-from app.database_manager import DatabaseManager
+# from app.config_manager import ConfigManager
+# from app.data_fetchers.s3_data_fetcher import S3DataFetcher
+# from app.data_fetchers.athena_data_fetcher import AthenaDataFetcher
+# from app.data_fetchers.database_data_fetcher import DatabaseDataFetcher
+# from app.data_fetchers.data_fetcher_router import DataFetcherRouter
+# from app.database_manager import DatabaseManager
 
 from app.power_curve.power_curve_manager import PowerCurveManager
 
 router = APIRouter()
 
-# Initialize ConfigManager
-config_manager = ConfigManager(
-    secret_arn_env_var="WINDWATTS_DATA_CONFIG_SECRET_ARN",
-    local_config_path="./app/config/windwatts_data_config.json") # replace with YOUR local config path
-athena_config = config_manager.get_config()
+# # Initialize ConfigManager
+# config_manager = ConfigManager(
+#     secret_arn_env_var="WINDWATTS_DATA_CONFIG_SECRET_ARN",
+#     local_config_path="./app/config/windwatts_data_config.json") # replace with YOUR local config path
+# athena_config = config_manager.get_config()
 
-# Initialize DataFetchers
-s3_data_fetcher = S3DataFetcher("WINDWATTS_S3_BUCKET_NAME")
-athena_data_fetcher = AthenaDataFetcher(athena_config=athena_config)
-db_manager = DatabaseManager()
-db_data_fetcher = DatabaseDataFetcher(db_manager=db_manager)
+# # Initialize DataFetchers
+# s3_data_fetcher = S3DataFetcher("WINDWATTS_S3_BUCKET_NAME")
+# athena_data_fetcher = AthenaDataFetcher(athena_config=athena_config)
+# db_manager = DatabaseManager()
+# db_data_fetcher = DatabaseDataFetcher(db_manager=db_manager)
 
-# Initialize DataFetcherRouter and register fetchers
-data_fetcher_router = DataFetcherRouter()
-data_fetcher_router.register_fetcher("database", db_data_fetcher)
-data_fetcher_router.register_fetcher("s3", s3_data_fetcher)
-data_fetcher_router.register_fetcher("athena", athena_data_fetcher)
+# # Initialize DataFetcherRouter and register fetchers
+# data_fetcher_router = DataFetcherRouter()
+# data_fetcher_router.register_fetcher("database", db_data_fetcher)
+# data_fetcher_router.register_fetcher("s3", s3_data_fetcher)
+# data_fetcher_router.register_fetcher("athena", athena_data_fetcher)
 
 # Load power curves
 power_curve_manager = PowerCurveManager("./app/power_curve/powercurves")
@@ -59,7 +60,14 @@ def get_windspeed(lat: float, lng: float, height: int, avg_type: str = 'global',
             "avg_type": avg_type
         }
 
-        data = data_fetcher_router.fetch_data(params, source=source)
+        # random data with 2 decimal places
+        average = round(random.uniform(1, 8), 2)
+
+        data = {
+            "global_avg": average,
+        }
+
+        # data = data_fetcher_router.fetch_data(params, source=source)
         if data is None:
             raise HTTPException(status_code=404, detail="Data not found")
         return data
@@ -85,20 +93,24 @@ def energy_production(lat: float, lng: float, height: int,
     Fetches the energy production for a given location, height, and power curve.
     """
     try:
-        params = {
-                "lat": lat,
-                "lng": lng,
-                "height": None,
-                "avg_type" : "none"
-                }
+        # params = {
+        #     "lat": lat,
+        #     "lng": lng,
+        #     "height": None,
+        #     "avg_type" : "none"
+        # }
+
+        df = {}
+
         # Retrieves full dataframe for a specific location from s3
-        df = data_fetcher_router.fetch_data(params,source=source)
+        # df = data_fetcher_router.fetch_data(params,source=source)
         if df is None:
             raise HTTPException(status_code=404, detail="Data not found")
 
-        yearly_avg_energy_production = power_curve_manager.fetch_yearly_avg_energy_production(df,height,selected_powercurve)
+        # yearly_avg_energy_production = power_curve_manager.fetch_yearly_avg_energy_production(df,height,selected_powercurve)
 
-        return {"energy_production": yearly_avg_energy_production['Average year']['kWh produced']}
+        # return {"energy_production": yearly_avg_energy_production['Average year']['kWh produced']}
+        return {"energy_production": random.randint(10000, 100000)} # placeholder until I can get local athena_config working
     except ValueError as ve:
         raise HTTPException(status_code=400, detail=str(ve))
     except Exception as e:
