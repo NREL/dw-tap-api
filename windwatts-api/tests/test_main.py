a2c0f4499d4e019babfef30240f2bc94356778e5
Shawn Petros <shawn.petros@gmail.com>
2025-03-17 15:06:43 -0700

    feat(windwatts-ui): add vitest
    add some vitest to the ui
    also update the api tests somewhat

--- windwatts-api/tests/test_main.py
+++ windwatts-api/tests/test_main.py
@@ -1,9 +1,12 @@
+import sys
 from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
 
+
 def test_read_root():
+    print(sys.executable)
     response = client.get("/healthcheck")
     assert response.status_code == 200
-    assert response.json() == { "status": "up" }
+    assert response.json() == {"status": "up"}
