8725362d3650f2ebc94e954e8e165e0243965e47
Roy Li <170121816+RLiNREL@users.noreply.github.com>
2025-06-15 22:51:12 -0600

    update out of bound link

--- windwatts-ui/src/components/shared/OutOfBoundsWarning.tsx
+++ windwatts-ui/src/components/shared/OutOfBoundsWarning.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 import WarningAmberIcon from "@mui/icons-material/WarningAmber";
 
 export default function OutOfBoundsWarning({ message }: { message: string }) {
@@ -24,6 +24,17 @@ export default function OutOfBoundsWarning({ message }: { message: string }) {
         </Typography>
         <Typography variant="body2" sx={{ whiteSpace: "pre-line", fontSize: 13 }}>
           {message}
+        </Typography>
+        <Typography variant="body2" sx={{ mt: 1, fontSize: 12 }}>
+          <Link
+            href="https://github.com/NREL/dw-tap-api/blob/master/about/era5.md#spatial-coverage"
+            target="_blank"
+            rel="noopener noreferrer"
+            underline="hover"
+            sx={{ color: "#856404", fontWeight: 500 }}
+          >
+            Learn more about spatial coverage
+          </Link>
         </Typography>
       </Box>
     </Box>
