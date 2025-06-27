441e0114803f2f229bf7cce36fa26c45a379571c
Roy Li <170121816+RLiNREL@users.noreply.github.com>
2025-06-27 15:16:38 -0600

    test branch protection-undo

--- windwatts-ui/src/components/Layout.tsx
+++ windwatts-ui/src/components/Layout.tsx
@@ -25,7 +25,7 @@ function Layout() {
               component="img"
               sx={{ height: 40 }}
               src="/assets/NREL-logo-reversed.png"
-              alt="NREL Log"
+              alt="NREL Logo"
             />
           </Toolbar>
         </AppBar>
