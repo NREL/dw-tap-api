73c5b3f9eaada56c457d59b4091a5242a6a7bd79
Shawn Petros <9226624+shawnpetros@users.noreply.github.com>
2025-07-11 10:44:12 -0700

    test

--- windwatts-ui/src/components/resultPane/AnalysisResults.tsx
+++ windwatts-ui/src/components/resultPane/AnalysisResults.tsx
@@ -1,18 +1,21 @@
-import { Box, Stack } from "@mui/material";
+import { 
+  Box, 
+  Stack } from "@mui/material";
 import ProductionCard from "./ProductionCard";
 import WindSpeedCard from "./WindSpeedCard";
 import WindResourceCard from "./WindResourceCard";
 
-const AnalysisResults = () => {
+const AnalysisResults =  () => {
   return (
-    <Stack spacing={2}>
+  <Stack spacing={2}>
       {/* Wind Speed and Wind Resource side-by-side */}
-      <Box sx={{ display: "flex", gap: 2 }}>
+       <Box sx={{ display: "flex", gap: 2 }}>
         <Box sx={{ flex: 1 }}>
-          <WindSpeedCard />
+           <WindSpeedCard />
         </Box>
         <Box sx={{ flex: 1 }}>
-          <WindResourceCard />
+          <WindResourceCard
+          />
         </Box>
       </Box>
 
