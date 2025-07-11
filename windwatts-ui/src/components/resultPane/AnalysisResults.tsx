697b0028afbaece885490dabcabd67c86e6b683b
Shawn Petros <9226624+shawnpetros@users.noreply.github.com>
2025-07-11 10:53:12 -0700

    fix precommit

--- windwatts-ui/src/components/resultPane/AnalysisResults.tsx
+++ windwatts-ui/src/components/resultPane/AnalysisResults.tsx
@@ -3,11 +3,14 @@ import {
   Stack } from "@mui/material";
 import ProductionCard from "./ProductionCard";
 import WindSpeedCard from "./WindSpeedCard";
-import WindResourceCard from "./WindResourceCard";
+import WindResourceCard 
+ from "./WindResourceCard";
 
-const AnalysisResults =  () => {
+const AnalysisResults =  () =>
+   {
   return (
   <Stack spacing={2}>
+
       {/* Wind Speed and Wind Resource side-by-side */}
        <Box sx={{ display: "flex", gap: 2 }}>
         <Box sx={{ flex: 1 }}>
