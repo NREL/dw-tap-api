d70d4ec33f5fb706fc118e734aa0cbec721fbeaa
Roy Li <RLiNREL@users.noreply.github.com>
2025-08-17 14:48:16 -0600

    clean-up to archived

--- misc/test-dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -X GET -H "Content-Type: application/json" -d @test.json https://dw-tap-dev.stratus.nrel.gov/batch
--- misc/test-local.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -X GET -H "Content-Type: application/json" -d @test.json localhost:8080/batch
