8c922d994d04810c2e882f42f0fbcf81f8e14647
Shawn Petros <9226624+shawnpetros@users.noreply.github.com>
2025-10-30 16:49:42 -0700

    feat(ui): port original header bar (AppBar color, title link, NREL logo)

--- windwatts-ui-ssr/src/components/Header.tsx
+++ windwatts-ui-ssr/src/components/Header.tsx
@@ -1,16 +1,35 @@
 "use client";
 
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Link as MuiLink } from "@mui/material";
+import Link from "next/link";
 
 export default function Header() {
+  const APP_TITLE = process.env.NEXT_PUBLIC_APP_TITLE || "Wind Watts";
   return (
-    <AppBar position="static" color="primary" enableColorOnDark>
-      <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          Wind Watts
-        </Typography>
-        <Box sx={{ opacity: 0.8, fontSize: 12 }}>NREL</Box>
+    <AppBar position="sticky" sx={{ bgcolor: "#0279c2" }}>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <MuiLink
+          href="/"
+          component={Link}
+          underline="none"
+          variant="h5"
+          sx={{ flexGrow: 1, color: "white" }}
+        >
+          {APP_TITLE}
+        </MuiLink>
+        <Box
+          component="img"
+          sx={{ height: 40 }}
+          src="/NREL-logo-reversed.png"
+          alt="NREL Logo"
+          onError={(e: any) => {
+            // hide if not present yet
+            e.currentTarget.style.display = "none";
+          }}
+        />
       </Toolbar>
     </AppBar>
   );
 }
+
+
