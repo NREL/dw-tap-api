13300ce41917f4befa169ddccdc412d0fdc291df
Roy Li <RLiNREL@users.noreply.github.com>
2025-11-11 13:22:19 -0700

    download button style

--- windwatts-ui/src/components/shared/DownloadButton.tsx
+++ windwatts-ui/src/components/shared/DownloadButton.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button } from "@mui/material";
+import { Button, Box } from "@mui/material";
 import DownloadIcon from "@mui/icons-material/Download";
 import { DownloadDialog } from "./DownloadDialog";
 
@@ -8,25 +8,27 @@ export const DownloadButton = () => {
 
   return (
     <>
-      <Button
-        variant="contained"
-        size="small"
-        startIcon={<DownloadIcon />}
-        onClick={() => setIsDialogOpen(true)}
-        sx={{
-          fontSize: "0.9em",
-          textTransform: "none",
-          borderRadius: 2,
-          px: 2,
-          py: 0.5,
-          backgroundColor: "primary.main",
-          "&:hover": {
-            backgroundColor: "primary.dark",
-          },
-        }}
-      >
-        Download Hourly Data
-      </Button>
+      <Box sx={{ display: "flex", justifyContent: "flex-end", m: 1 }}>
+        <Button
+          variant="contained"
+          size="small"
+          startIcon={<DownloadIcon />}
+          onClick={() => setIsDialogOpen(true)}
+          sx={{
+            fontSize: "0.9em",
+            textTransform: "none",
+            borderRadius: 2,
+            px: 2,
+            py: 0.5,
+            backgroundColor: "primary.main",
+            "&:hover": {
+              backgroundColor: "primary.dark",
+            },
+          }}
+        >
+          Download Hourly Data
+        </Button>
+      </Box>
 
       {isDialogOpen && (
         <DownloadDialog onClose={() => setIsDialogOpen(false)} />
